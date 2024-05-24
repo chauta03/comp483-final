65a3b5b691d2f45316b8066371c7645620934d80
colek <colekoryto17@gmail.com>
2024-05-24 13:06:25 -0400

    Add decrypt_vote

--- src/CTF.py
+++ src/CTF.py
@@ -4,16 +4,29 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
+class CTF:
 
+    __private_key = None
 
-def setup_RSA():
+    def setup_RSA(self):
 
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    self.public_key = private_key.public_key()
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.public_key = private_key.public_key()
 
-def get_public_key():
-    return self.public_key
+    def get_public_key(self):
+        return self.public_key
+
+    def decrypt_vote(self, encry_vote):
+        recovered_vote = self.__private_key.decrypt(
+            encry_vote,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None  # rarely used. Just leave it 'None'
+            ))
+
+        return recovered_vote
