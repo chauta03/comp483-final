7403c5ac99614de7c2493ec7cfc01d6661e7bb9b
colek <colekoryto17@gmail.com>
2024-05-24 12:44:12 -0400

    set up public key gen

---
+++ src/CTF.py
@@ -0,0 +1,19 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+class CTF:
+    def setup_RSA(self):
+
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.public_key = private_key.public_key()
+
+    def get_public_key(self):
+        return self.public_key
