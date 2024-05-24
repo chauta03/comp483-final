a43da41f189ac00ec4e2776b147c962c06c71e6f
colek <colekoryto17@gmail.com>
2024-05-24 12:59:10 -0400

    Add create_vote

--- src/voter.py
+++ src/voter.py
@@ -1,6 +1,11 @@
 import CLA
 import CTF
 import random
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 
 class Voter:
@@ -12,4 +17,16 @@ class Voter:
         self.my_ID = random.randint(0, 2 ** 32 - 1)
 
     def create_vote(self):
+        vote = "John"
+        message = f"{self.my_ID},{self.validation_num},{vote}"
 
+        ciphertext1 = self.CTF_public_key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None  # rarely used. Just leave it 'None'
+            )
+        )
+
+        return ciphertext1
