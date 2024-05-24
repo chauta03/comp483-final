df5b1a499c903f9c687eb0ae62f512821923d98d
tachau03 <taminhchau8903@gmail.com>
2024-05-24 12:43:35 -0400

    get_validation_number_set

--- src/CLA.py
+++ src/CLA.py
@@ -5,7 +5,13 @@ validation_number_set = set()
 def random_validation_number():
     while True:
         number = random.randint(0, 2 ** 32 - 1)
+
+        if len(random_validation_number) >= 2 ** 32:
+            raise Exception("No more validation numbers available")
+
         if number not in validation_number_set:
             validation_number_set.add(number)
             return number
-    return 
+
+def get_validation_number_set():
+    return validation_number_set
