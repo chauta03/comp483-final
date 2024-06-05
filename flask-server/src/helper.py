cde4639818b7188b2304d70e9fe911abb9bf6e81
tachau03 <taminhchau8903@gmail.com>
2024-06-04 23:53:49 -0400

    helper function file for database

---
+++ flask-server/src/helper.py
@@ -0,0 +1,24 @@
+import sqlite3
+from flask import g
+from flask import Flask
+
+DATABASE = 'database.db'
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
