bed0b5369421b3212dd5cb1a729f0b79954500c5
tachau03 <taminhchau8903@gmail.com>
2024-06-04 23:54:05 -0400

    voter.py

--- flask-server/src/voter.py
+++ flask-server/src/voter.py
@@ -6,42 +6,17 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 import AES
 
-
-# Class that controls all functions for a voter
-class Voter:
-
-    # Initializes voter with ID along with RSA encryption
-    def __init__(self):
-        self.my_ID = random.randint(0, 2 ** 32 - 1)
-        self.validation_num = None
-        self.private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-            backend=default_backend()
-        )
-        self.public_key = self.private_key.public_key()
-
-
-    # Getter function for RSA public key
-    def get_public_key(self):
-        return self.public_key
-
-    # Get id number
-    def get_id(self):
-        return self.my_ID
-
-    # Updates used validation number by decrypting validation number using RSA encrypted AES keys
-    def set_validation_num(self, en_validation_num, en_AES_key, en_AES_iv):
+def set_validation_num(private_key, en_validation_num, en_AES_key, en_AES_iv):
 
         # Decrypts AES key and IV with RSA
-        AES_key = self.private_key.decrypt(
+        AES_key = private_key.decrypt(
             en_AES_key,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
                 algorithm=hashes.SHA256(),
                 label=None  # rarely used. Just leave it 'None'
             ))
-        AES_iv = self.private_key.decrypt(
+        AES_iv = private_key.decrypt(
             en_AES_iv,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -53,39 +28,40 @@ class Voter:
         AES_manager = AES.EncryptionManager(AES_key, AES_iv)
         temp_message = AES_manager.update_decryptor(en_validation_num)
         temp_message += AES_manager.finalize_decryptor()
-        self.validation_num = int(temp_message.decode('utf-8'))
+        
+        validation_num = int(temp_message.decode('utf-8'))
+        return validation_num
 
+# Given a public RSA key, encrypts CLA AES key with RSA public key and returns RSA encrypted AES key along with AES encrypted validation number
+def get_encrypted_vote(my_ID, validation_num, vote, CTF_public_key):
 
-    # Given a public RSA key, encrypts CLA AES key with RSA public key and returns RSA encrypted AES key along with AES encrypted validation number
-    def get_encrypted_vote(self, vote, CTF_public_key):
+    # Creates random AES key and IV
+    AES_key = os.urandom(32)
+    AES_iv = os.urandom(16)
 
-        # Creates random AES key and IV
-        AES_key = os.urandom(32)
-        AES_iv = os.urandom(16)
+    # Creates and encrypts message to send with AES
+    message = f"{my_ID},{validation_num},{vote}".encode('utf-8')
+    AES_manager = AES.EncryptionManager(AES_key, AES_iv)
+    en_vote = AES_manager.update_encryptor(message)
+    en_vote += AES_manager.finalize_encryptor()
 
-        # Creates and encrypts message to send with AES
-        message = f"{self.my_ID},{self.validation_num},{vote}".encode('utf-8')
-        AES_manager = AES.EncryptionManager(AES_key, AES_iv)
-        en_vote = AES_manager.update_encryptor(message)
-        en_vote += AES_manager.finalize_encryptor()
-
-        # Encrypts AES key and IV with RSA
-        en_AES_key = CTF_public_key.encrypt(
-            AES_key,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None  # rarely used. Just leave it 'None'
-            )
+    # Encrypts AES key and IV with RSA
+    en_AES_key = CTF_public_key.encrypt(
+        AES_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None  # rarely used. Just leave it 'None'
         )
-        en_AES_iv = CTF_public_key.encrypt(
-            AES_iv,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None  # rarely used. Just leave it 'None'
-            )
+    )
+    en_AES_iv = CTF_public_key.encrypt(
+        AES_iv,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None  # rarely used. Just leave it 'None'
         )
+    )
 
-        # Returns encrypted vote and AES information
-        return en_vote, en_AES_key, en_AES_iv
+    # Returns encrypted vote and AES information
+    return en_vote, en_AES_key, en_AES_iv
